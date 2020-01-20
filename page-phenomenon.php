5b7ba86e460697a5ba8ba9b8923ebfa129273ec3
Brian Coords <bacoords@gmail.com>
2020-01-19 20:11:23 -0800

    updates template

--- page-phenomenon.php
+++ page-phenomenon.php
@@ -12,7 +12,8 @@ get_header();
 			if ( have_posts() ) :
 				while ( have_posts() ) :
 					the_post();
-					get_template_part( 'content', 'page' );
+					the_content();
+					// get_template_part( 'content', 'page' );
 				endwhile;
 			endif;
 			?>
